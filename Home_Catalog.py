a26800ed7db0a3f688d882ee75944d1a250fff28
Martina <martina.faggiano@gmail.com>
2021-12-03 16:59:34 +0100

    Home_catalog inizio

---
+++ Home_Catalog.py
@@ -0,0 +1,21 @@
+import cherrypy
+
+class Calcoliamo(object):
+
+    exposed = True
+    def GET(self, *uri, **params):
+        if params!={} and len(uri)!=0:  
+            keyList = list(params.keys())
+            Valuelist = [params[key] for key in params.keys()]
+
+            return Valuelist
+
+    
+if __name__=="__main__":
+    conf={
+        '/':{
+            'request.dispatch' : cherrypy.dispatch.MethodDispatcher(),
+            'tool.session.on' : True
+        }
+    }
+    cherrypy.quickstart(Calcoliamo(), '/', conf)
