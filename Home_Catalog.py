47a31f5fc0db6c78c2e9526ca66a2e78204abfdf
Martina <martina.faggiano@gmail.com>
2021-12-03 18:11:55 +0100

    Bozza  Home Catalog. problema sulla doppia get

--- Home_Catalog.py
+++ Home_Catalog.py
@@ -1,14 +1,48 @@
 import cherrypy
+import json
 
-class Calcoliamo(object):
+
+class Device():
+    def __init__(self, id, name, measure, unit):
+        self.id = id
+        self.name = name
+        self.measure = measure
+        self.unit = unit
+
+class HomeCatalog(object):
 
     exposed = True
-    def GET(self, *uri, **params):
-        if params!={} and len(uri)!=0:  
-            keyList = list(params.keys())
-            Valuelist = [params[key] for key in params.keys()]
+    def GET(self, *uri):
+        if uri[0]=="devicesList":
+            data = json.load(open("devices.json"))
+            self.devices = []
+            for device in data["devicesList"]:
+                # Device(device["deviceID"], device["deviceName"], device["measureType"], device["unit"])
+                self.devices.append(device)
+            return self.devices
 
-            return Valuelist
+
+    # def GET(self, *uri):
+    #     if uri[0]=="topicsList":
+    #         data = json.load(open("MQTT-topics.json"))
+    #         self.topics = []
+    #         for topic in data["Topics"]:
+    #             self.topics.append(topic)
+    #         return self.topics
+
+    def POST(self, id, name, type, unit):
+        print(id, name, type, unit)
+
+        data = cherrypy.request.body.read()
+        data = json.loads(data)
+        self.devices.append(data)
+        print(self.devices)
+
+        json_file = json.load(open("devices.json"))
+        json_file["devicesList"] = self.devices
+        print(json_file)
+        with open("devices.json", "w") as pippo:
+            json.dump(json_file, pippo)
 
     
 if __name__=="__main__":
@@ -18,6 +52,4 @@ if __name__=="__main__":
             'tool.session.on' : True
         }
     }
-    cherrypy.quickstart(Calcoliamo(), '/', conf)
-    
-    #ciao mi chiamo martina
+    cherrypy.quickstart(HomeCatalog(), '/', conf)
